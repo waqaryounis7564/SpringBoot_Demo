d0a9f06050b0c01aced6a51a4d9ff759acf14f57
Waqar <waqar.younas7564@gmail.com>
2020-12-11 09:11:00 +0500

    nysql integration with springboot jdbc

--- src/main/java/com/example/demo/DemoSpringBootApplication.java
+++ src/main/java/com/example/demo/DemoSpringBootApplication.java
@@ -3,11 +3,34 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/sql_hr?user=root&password=admin")) {
+
+                System.out.println("database connected :" + connection.getMetaData().getDatabaseProductName());
+
+                try (Statement statement = connection.createStatement()) {
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM sql_hr.employees");
+                    while (resultSet.next()) {
+                        names.add(resultSet.getString(2));
+
+                    }
+                }
+            }
+
+            names.stream().filter(el -> el.contains("Ivy")).forEach(System.out::println);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(ex.fillInStackTrace());
+
+        }
+    }
 
 }
